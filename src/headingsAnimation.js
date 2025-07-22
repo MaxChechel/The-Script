21fd4adc425f8776833386ffd13ef11898941497
Max Chechel <maksymchechel@yahoo.com>
2025-07-22 10:37:29 +0200

    Update headingsAnimation.js

--- src/headingsAnimation.js
+++ src/headingsAnimation.js
@@ -11,7 +11,6 @@ function createGradientText(element) {
   // Add text wrapping styles for Safari compatibility
   Object.assign(element.style, {
     textWrap: "wrap",
-    wordWrap: "break-word",
     overflowWrap: "break-word",
   });
 
